f28c7ab5fb10ec5a81c151bf0e2b23ce564e8bd1
Fahmi Zidan <zidanf656@gmail.com>
2025-11-07 07:07:55 +0700

    add footer

---
+++ layout/footer.php
@@ -0,0 +1,16 @@
+<?php
+// client/layout/footer.php
+?>
+</main>
+
+<footer class="p-4 text-center text-sm text-gray-500 border-t border-gray-200 mt-auto">
+    &copy; <?php echo date('Y'); ?> Sistem Manajemen Tugas.
+</footer>
+
+</div>
+</div>
+<script src="/assets/js/script.js"></script>
+
+</body>
+
+</html>
